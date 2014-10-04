efe51a956a84e1198c8ba7799ed9cb4456bad481
Peter Belica <atmelatmel@gmail.com>
2014-10-04 15:44:49 +0200

    Error correction and code cleaning

--- irobot/irobot.py
+++ irobot/irobot.py
@@ -2,11 +2,10 @@
 """ Published under GNU GENERAL PUBLIC LICENSE (see LICENSE)    """
 import serial
 import threading
-from PyQt4 import QtCore, QtGui, uic
-from PyQt4.QtCore import QObject, pyqtSignal
 import sys
 from time import sleep  
-
+from PyQt4 import QtCore, QtGui, uic
+from PyQt4.QtCore import QObject, pyqtSignal
 
 
 class iRobotSensors:
@@ -19,8 +18,8 @@ class iRobotSensors:
     __voltage = 0
     __current = 0
     __wallSignal = 0
-    
     valueChanged = pyqtSignal()
+    
     def bumpLeft(self):
         return self.__bumpLeft
     def bumpRight(self):
@@ -44,8 +43,6 @@ class iRobotSensors:
     def parseStream(self, data):
         i=1
         while i < ord(data[0]):
-#             lenData = getLenOfData(data[i])
-
             # PACKET ID
             pid = ord(data[i])
             
@@ -54,7 +51,7 @@ class iRobotSensors:
             if pid == 7:
                 self.__bumpRight = ord(data[i])&(0x01)
                 self.__bumpLeft = (ord(data[i])&(0x02))>>1
-                print("Bump Left: " + str(self.__bumpLeft) + " Bump Right: " + str(self.bumpRight()))
+                # print("Bump Left: " + str(self.__bumpLeft) + " Bump Right: " + str(self.__bumpRight()))
             elif pid == 8:
                 __wall = ord(data[i])
             elif pid == 9:
@@ -87,14 +84,14 @@ class iRobotSensors:
                 pass
             elif pid == 22:
                 self.__voltage = ord(data[i])*256 + ord(data[i+1]) 
-                print("Voltage: " + str(self.__voltage)) 
+                # print("Voltage: " + str(self.__voltage)) 
                 i += 1
             elif pid == 23:
-#                 if ord(data[i])&0x80 != 0:
-#                     self.__current = ((ord(data[i])&0x7f)*256 + ord(data[i+1]))
-#                 else:
-#                     self.__current = -1*((ord(data[i])&0x7f)*256 + ord(data[i+1]))
-                print("Current: " + str(self.__current)) 
+                if ord(data[i])&(0x80) != 0:    # negative current
+                    self.__current = -1*((ord(data[i])&(0x7f))*256 + ord(data[i+1]))
+                else:   # positive current
+                    self.__current = ord(data[i])&(0x7f)*256 + ord(data[i+1])
+                # print("Current: " + str(self.__current)) 
                 i += 1
             elif pid == 24:
                 pass
@@ -106,7 +103,7 @@ class iRobotSensors:
 #                 self.valueChanged.emit()
                 
                 self.__wallSignal = ord(data[i])<<8 + ord(data[i+1])
-                print("Wall signal: " + str(self.__wallSignal)) 
+                # print("Wall signal: " + str(self.__wallSignal)) 
                 i += 1
             elif pid == 28:
                 i += 1
@@ -139,97 +136,98 @@ class iRobotSensors:
             elif pid == 42:
                 i += 1
             else :
-                print('Wrong packet ID: ' + str(pid))
+                print('ERROR  Wrong packet ID: ' + str(pid))
                 return
                 
             i += 1
-         
+     
 class iRobot():
     __port = 0
     __packetLength = 0
     __streamRunning = 0
-    sensors = 0
     __packetLengthSemaphore = 0
+    sensors = 0
+    
     def __init__(self, path):
         self.sensors = iRobotSensors()
-#         super(iRobot, self).__init__()
-        self.__packetLengthSemaphore = threading.Semaphore()
+#       super(iRobot, self).__init__()
         self.__port = serial.Serial(path, baudrate=57600)
         self.__port.open()
-    
+        self.__packetLengthSemaphore = threading.Semaphore()
+
     def modeSafe(self):
         self.__port.write("\x80\x83")
-        
-    def go(self):
-        self.__port.write("\x91\x01\xff\x01\xff")
     def stop(self):
         self.__port.write("\x91\x00\x00\x00\x00")
         self.__port.write("\x80")
-    def streamStart(self):
-        self.__port.write("\x94\x04\x07\x1B\x16\x17")
+ 
+    def streamStart(self, requestedPacketsString):
+        self.__port.write(requestedPacketsString)
     def streamRead(self):
-        self.streamStart()
         self.__streamRunning = True
         while self.__streamRunning:
-            if self.__packetLength == 0:
-                
+            if self.__packetLength == 0:    # get synchronized
                 sync = ord(self.__port.read(1))
-                print("SYNCH: " + str(sync))
                 if sync == 19:
                     self.__packetLengthSemaphore.acquire()
                     self.__packetLength = ord(self.__port.read(1))
-                    print("packetLen: " + str(self.__packetLength))
                     if (self.__packetLength != 0):
                         trash = self.__port.read(self.__packetLength+1)
-                        for i in range(self.__packetLength):
-                            print(str(ord(trash[i])) + " ")
                     self.__packetLengthSemaphore.release()
             elif self.__packetLength > 0:
                 sync = ord(self.__port.read(1))
-                print(sync)
                 if sync == 19:
                     self.__packetLengthSemaphore.acquire()
                     data = self.__port.read(self.__packetLength+2)
-                    # TODO Calculate checksum
-                    for i in range(self.__packetLength):
-                        print(str(ord(data[i])) + " ")
-                    
                     self.__packetLengthSemaphore.release()
-                    print("PACKETLEN" + str(ord(data[0])))
+                    # checksum calculations START
+                    sum = 0
+                    for i in range(0, len(data)):
+                        sum += ord(data[i])
+                    if sum&(0xff) != 0:     # bad checksum
+                        self.__packetLength = 0
+                        continue
+                    # checksum calculations END
                     self.sensors.parseStream(data)
                 else :
                     self.__packetLength = 0
-        print("exitstream")
-                
+        print("iRobot Stream read thread Stopped")
     def streamStop(self):
         self.__port.write("\x96\x00")
         self.__streamRunning = False
+
     def __del__(self):
         self.streamStop()
+        self.stop()
         self.__port.close()
-        
+     
 class mainWidget(QtGui.QWidget):
     __robot = 0
     
     def __init__(self):
         super(mainWidget, self).__init__()
-        uic.loadUi('untitled.ui',self)
+        uic.loadUi('iRobotGUI.ui',self)
         self.__robot = iRobot("/dev/rfcomm11")
 #         self.__robot.sensors.valueChanged.connect(self.slotSensorsChanged)
         self.show()
+        
     def slotStart(self):
         self.__robot.modeSafe()
+        print("iRobot Control Enabled")
     def slotStop(self):
-        self.__robot.stop()
         self.__robot.streamStop()
-        print('stop')
+        self.__robot.stop()
+        print("iRobot Control Disabled")
+        
     def slotTask1Start(self):
+        self.streamStart("\x94\x04\x07\x1B\x16\x17")    # bumps, wallSignal, voltage, current
         self.__threadComm = threading.Thread(target=self.__robot.streamRead)
         self.__threadComm.start()
     def slotTask1Stop(self):
         self.__robot.streamStop()
     def slotSensorsChanged(self):
-        print('Changed')
+        print("iRobot Sensor data changed")
+             
              
 app = QtGui.QApplication(sys.argv)
 w = mainWidget()
